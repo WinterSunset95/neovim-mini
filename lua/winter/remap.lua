ade71c11798a200e2d6b1b0beccf2fce334d4726
WinterSunset95 <wintersunset95@gmail.com>
2023-08-01 11:28:37 +0530

    new remap

--- lua/winter/remap.lua
+++ lua/winter/remap.lua
@@ -5,12 +5,15 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("n", "<C-j>", "<C-d>")
 vim.keymap.set("n", "<C-k>", "<C-u>")
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>')
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>')
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>')
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>')
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
 
 -- autopairs
 vim.keymap.set("i", '"', '""<left>')
