90d7639a5f33cae7531a83e27867932af8b3069b
WinterSunset95 <wintersunset95@gmail.com>
2023-08-14 13:10:12 +0530

    new commit

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -1,4 +1,7 @@
 local nvim_lsp = require('lspconfig')
+nvim_lsp.jdtls.setup {
+	cmd = { "jdtls" }
+}
 nvim_lsp.clangd.setup {
 	cmd = { "clangd", "--background-index" }
 }
--- lua/winter/remap.lua
+++ lua/winter/remap.lua
@@ -11,6 +11,7 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '<F8>', ':tabnew | term make file=#<cr>i')
 
 -- autopairs
 vim.keymap.set("i", '"', '""<left>')
