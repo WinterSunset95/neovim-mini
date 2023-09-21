599767dbe1256ba2a822f5d70d8e24b34e97882c
WinterSunset95 <wintersunset95@gmail.com>
2023-09-21 19:21:17 +0530

    added new remaps

--- lua/winter/remaps.lua
+++ lua/winter/remaps.lua
@@ -30,27 +30,5 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', '<F8>', ':lua build()<cr>')
---vim.keymap.set('n', '<F8>', ':tabnew | term make file=#<cr>i')
-
--- autopairs
--- vim.keymap.set("i", '"', '""<left>')
--- vim.keymap.set("i", "'", "''<left>")
--- vim.keymap.set("i", "(", "()<left>")
--- vim.keymap.set("i", "[", "[]<left>")
--- vim.keymap.set("i", "{", "{}<left>")
--- vim.keymap.set("i", "`", "``<left>")
--- vim.keymap.set("i", "<", "<><left>")
--- 
--- vim.keymap.set("i", '"""', '""')
--- vim.keymap.set("i", "'''", "''")
--- vim.keymap.set("i", "()", "()")
--- vim.keymap.set("i", "[]", "[]")
--- vim.keymap.set("i", "{}", "{}")
--- vim.keymap.set("i", "``", "``")
--- vim.keymap.set("i", "<>", "<>")
--- 
--- vim.keymap.set("i", "```", "``````<left><left><left>")
--- 
--- vim.keymap.set("i", "{<cr>", "{<cr>}<esc>O")
 
 vim.api.nvim_set_keymap('i', '<CR>', 'pumvisible() ? "<C-y>" : "<CR>"', { noremap = true, expr = true })
