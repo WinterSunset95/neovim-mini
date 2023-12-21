833dec0977da87a5a210ec37ddf55f2018eaddba
WinterSunset95 <wintersunset95@gmail.com>
2023-12-21 19:03:59 +0530

    I have no idea what changes I made

--- lua/winter/packer.lua
+++ lua/winter/packer.lua
@@ -5,8 +5,16 @@ return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 	-- The only theme I like
 	use 'morhetz/gruvbox'
-	-- Well.... I like it
+	-- Been quite useful for me
 	use 'github/copilot.vim'
 	-- Lsp stuff
 	use {'neoclide/coc.nvim', branch = 'release'}
+	-- It's a duck
+	use {
+		'tamton-aquib/duck.nvim',
+		config = function()
+			vim.keymap.set('n', '<leader>dd', function() require("duck").hatch() end, {})
+			vim.keymap.set('n', '<leader>dk', function() require("duck").cook() end, {})
+		end
+	}
 end)
