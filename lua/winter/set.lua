66e7aa12fa7c942b159e12a3923f154e9afb21cb
WinterSunset95 <wintersunset95@gmail.com>
2024-05-29 22:01:12 +0530

    new setting

--- lua/winter/set.lua
+++ lua/winter/set.lua
@@ -1,11 +1,12 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
+vim.opt.autoindent = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = false
-vim.opt.smartindent = true
+--vim.opt.smartindent = true
 vim.opt.termguicolors = true
 vim.opt.completeopt = "menu,menuone,noinsert"
 vim.opt.foldmethod = "indent"
